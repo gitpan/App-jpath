631d2185bfc49a8533b8cfc6b34cb96e5eb20847
Steven Haryanto <stevenharyanto@gmail.com>
2014-02-14 07:56:21 +0000

    Import of SHARYANTO/App-jpath-0.02 from CPAN.
    gitpan-cpan-distribution: App-jpath
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHARYANTO/App-jpath-0.02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/App/jpath.pm
+++ lib/App/jpath.pm
@@ -1,6 +1,6 @@
 package App::jpath;
 
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 
 1;
 # ABSTRACT: Command-line tool for JSON::Path
@@ -17,7 +17,7 @@ App::jpath - Command-line tool for JSON::Path
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 SYNOPSIS
 
